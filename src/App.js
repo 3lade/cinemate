f03bb12a81a00905e9c41205542ff661edb80481
3lade <154913148+3lade@users.noreply.github.com>
2026-01-23 00:27:46 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { AllRoutes } from "./routes/AllRoutes";
 import { Header, Footer} from "./components"
 import './App.css';
 
+console.log(`API KEY: ${process.env.REACT_APP_API_KEY}`);
+
 function App() {
   return (
     <div className="App">
